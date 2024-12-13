bac407d7d9abec0c37c9fbd039917f2a9edc3866
yuhtng12 <gia.huy.tong1@gmail.com>
2024-12-13 23:18:52 +0100

    COrs origin hinzugefügt

--- src/main/java/de/htwberlin/webtech/web/LebensmittelController.java
+++ src/main/java/de/htwberlin/webtech/web/LebensmittelController.java
@@ -1,11 +1,11 @@
 package de.htwberlin.webtech.web;
 
-
 import de.htwberlin.webtech.model.Lebensmittel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +13,7 @@ import java.util.List;
 @Controller
 public class LebensmittelController {
 
-    private List<Lebensmittel> lebensmittelListe = new ArrayList<>();
+    private final List<Lebensmittel> lebensmittelListe = new ArrayList<>();
 
     public LebensmittelController() {
         lebensmittelListe.add(new Lebensmittel("Apfel", 52));
@@ -21,10 +21,14 @@ public class LebensmittelController {
         lebensmittelListe.add(new Lebensmittel("Kartoffel", 77));
     }
 
-    @GetMapping(path = "/api/lebensmittel") //funktioniert noch nicht, Fehlermeldung unbekannt
+    @CrossOrigin(origins = "https://frontend-kalorienzaehler.onrender.com")
+    @GetMapping(path = "/api/lebensmittel")
+    @ResponseBody
     public ResponseEntity<List<Lebensmittel>> getLebensmittelListe() {
-        return ResponseEntity.ok(lebensmittelListe);
+        return ResponseEntity.ok()
+                .header("Cache-Control", "no-cache, no-store, must-revalidate")
+                .header("Pragma", "no-cache")
+                .header("Expires", "0")
+                .body(lebensmittelListe);
     }
 }
-
-
