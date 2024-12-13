4575c9101fd59025059b0fcc019af07694eb34a6
yuhtng12 <gia.huy.tong1@gmail.com>
2024-12-13 16:41:51 +0100

    Finales Config

--- src/main/java/de/htwberlin/webtech/config/WebConfig.java
+++ src/main/java/de/htwberlin/webtech/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://frontend-kalorienzaehler.onrender.com/")
+                        .allowedOrigins("https://frontend-kalorienzaehler.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
