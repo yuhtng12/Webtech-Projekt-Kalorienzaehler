2ea41b96147f34986324fb1d8c9b82beb2b6df20
yuhtng12 <gia.huy.tong1@gmail.com>
2024-12-13 15:12:18 +0100

    CrossOrigin hinzugefuegt

--- src/main/java/de/htwberlin/webtech/web/LebensmittelController.java
+++ src/main/java/de/htwberlin/webtech/web/LebensmittelController.java
@@ -4,11 +4,13 @@ package de.htwberlin.webtech.web;
 import de.htwberlin.webtech.model.Lebensmittel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "https://webtech-frontend.onrender.com")
 @Controller
 public class LebensmittelController {
 
